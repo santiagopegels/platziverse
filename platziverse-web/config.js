c02d57dbaeb92074a15926057a02aa13a928b431
Santiago Pegels <santiagopegels@gmail.com>
2019-07-25 15:18:25 -0300

    Cambios para produccion

--- platziverse-web/config.js
+++ platziverse-web/config.js
@@ -3,5 +3,6 @@
 module.exports = {
   endpoint: process.env.API_ENDPOINT || 'http://localhost:3050',
   serverHost: process.env.SERVER_HOST || 'http://localhost:8080',
+  mqttHost: process.env.MQTT_HOST || 'mqtt://localhost',
   apiToken: process.env.API_TOKEN || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InBsYXR6aSIsImFkbWluIjp0cnVlLCJwZXJtaXNzaW9ucyI6WyJtZXRyaWNzOnJlYWQiXSwiaWF0IjoxNTYxNjcxNjU3fQ.5_0S-GBaDU9TSlAgN8mXk8q3fU2FHD8HljmkEizHyao'
 }
